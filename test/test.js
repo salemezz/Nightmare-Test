f065394c42a19123aa45e9b51fc419c444da9ea6
Sal <salem.ezz@gmail.com>
2019-01-17 13:33:31 -0500

    first commmit

---
+++ test/test.js
@@ -0,0 +1,38 @@
+var Nightmare = require("nightmare");
+var expect = require("chai").expect;
+
+describe("Bushel 44", function() {
+
+  this.timeout(30000);
+  it("should send user to product page", function(done) {
+
+    Nightmare({ show: true })
+      .goto("https://bushel44-ui.herokuapp.com")
+      .click("a[href='/products']")
+      .evaluate(function() {
+        return document.title;
+      })
+      .then(function(title) {
+        expect(title).to.equal("Bushel 44 | Products");
+        done();
+      });
+  });
+
+  it("should present a link to user portal after login", function(done) {
+    new Nightmare({ show: true })
+      .goto("https://bushel44-ui.herokuapp.com/login")
+      .type("#username", "sal")
+      .type("#password", "s")
+      .click("#login_button")
+      .evaluate(function() {
+        return document.querySelector("a[href='/protected']");
+      })
+      .then(function(link) {
+        expect(link).to.not.equal(undefined);
+        done();
+      });
+  });
+  // it("should throw an error for fun", function() {
+  //   throw new Error("Failed on purpose, just to make the Mocha output more interesting.");
+  // });
+});
